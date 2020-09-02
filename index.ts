da28ef1a2bcff31a7ede6ef82a5d21c7f5c1edb9
Anwesh43 <anweshthecool0@gmail.com>
2020-09-02 16:44:08 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const strokeFactor : number = 90
 const sizeFactor : number = 4.8 
 const delay : number = 90 
 const foreColor : string = "#bdbdbd"
-const color : Array<string> = ["#F44336", "#3F51B5", "#009688", "#2196F3", "#4CAF50"]
+const colors : Array<string> = ["#F44336", "#3F51B5", "#009688", "#2196F3", "#4CAF50"]
 const rot : number = Math.PI / 2  
 
 class ScaleUtil {
@@ -21,5 +21,39 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLRotCreator(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, 2)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, 2)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.rotate(Math.PI / 2 * sf2 * j)
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * (sf1 + (1 - j) * sf2))
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLRCNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawLRotCreator(context, scale)
+    
     }
 }
