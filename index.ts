8f010e6651cfa334d0f0744907ef0eeb05449ba8
Anwesh43 <anweshthecool0@gmail.com>
2020-09-02 16:52:52 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const scGap : number = 0.02 / parts
 const strokeFactor : number = 90
 const sizeFactor : number = 4.8 
 const delay : number = 90 
-const foreColor : string = "#bdbdbd"
+const backColor : string = "#bdbdbd"
 const colors : Array<string> = ["#F44336", "#3F51B5", "#009688", "#2196F3", "#4CAF50"]
 const rot : number = Math.PI / 2  
 
@@ -55,5 +55,36 @@ class DrawingUtil {
         context.strokeStyle = colors[i]
         DrawingUtil.drawLRotCreator(context, scale)
     
+    }
+}
+
+class Stage {
+    
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor 
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+            
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
